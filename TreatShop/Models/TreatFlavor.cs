5354c8c4e10b2b67efa0322208f41d0e9b47b624
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-27 08:57:22 -0700

    add join model for Treat Flavor

---
+++ TreatShop/Models/TreatFlavor.cs
@@ -0,0 +1,13 @@
+namespace TreatShop.Models
+{
+  public class TreatFlavor
+    {       
+        public int TreatFlavorId { get; set; }
+        public int TreatId { get; set; }
+        public int FlavorId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public Treat Treat { get; set; }
+        public Flavor Flavor { get; set; }
+    }
+}
