267c67d43d52165e84950b1e3f0a0116c683e6fb
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-31 00:32:55 -0700

    add treat list to splash page

--- TreatShop/Controllers/HomeController.cs
+++ TreatShop/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
 
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TreatShop.Models;
 
 namespace TreatShop.Controllers
 {
   public class HomeController : Controller
   {
+
+    private readonly TreatShopContext _db;
+
+    public HomeController(TreatShopContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      return View(_db.Treats.ToList());
     }
   }
 }
